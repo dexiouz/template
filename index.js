fabab10d54a62d268bbb21ca8c08db4ce145d8b4
Chidera Paul Ugwuanyi <dexiouz@gmail.com>
2022-07-13 22:12:24 +0100

    - apollo client

--- index.js
+++ index.js
@@ -1,10 +1,52 @@
 /**
  * @format
  */
+import React from 'react';
 import 'react-native-gesture-handler';
 import { AppRegistry } from 'react-native';
-import App from './App';
 import AppRoute from './src/navigation';
 import { name as appName } from './app.json';
+import {
+    ApolloClient,
+    InMemoryCache,
+    createHttpLink,
+    ApolloProvider,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-AppRegistry.registerComponent(appName, () => AppRoute);
+const httpLink = createHttpLink({
+    uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+    // get the authentication token from local storage if it exists
+    const token = AsyncStorage.getItem('token');
+    // return the headers to the context so httpLink can read them
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? token : '',
+        },
+    };
+});
+
+const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+    defaultOptions: {
+        watchQuery: {
+            fetchPolicy: 'cache-and-network',
+        },
+    },
+});
+
+const App = () => (
+    <SafeAreaProvider>
+        <ApolloProvider client={client}>
+            <AppRoute />
+        </ApolloProvider>
+    </SafeAreaProvider>
+);
+AppRegistry.registerComponent(appName, () => App);
